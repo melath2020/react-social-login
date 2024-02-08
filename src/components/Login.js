df507d744a29c1a6aa71f380067b56b49ccbec95
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-08 14:25:45 +0530

    update

---
+++ src/components/Login.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './loginstyle.css'
+import { GoogleLogin } from '@react-oauth/google';
+const Login = () => {
+  return (
+    <div>
+        <div class="container box-width shadow-sm pb-5" >
+            <div className="text-center">
+                <h5 className='mt-5 pt-3 mb-3'>Login Here</h5>
+                <div className="box bg-light-50  p-2 ">
+                    <input type="text" className='form-control' placeholder='UserName'/>
+                    <input type="text" className='form-control mt-3' placeholder='Password'/>
+                </div>
+                <hr />
+                <GoogleLogin
+  onSuccess={credentialResponse => {
+    console.log(credentialResponse);
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+/>;
+                <button className='google-btn'>Login With Google</button>
+            </div>
+          
+        </div>
+        
+      
+    </div>
+  )
+}
+
+export default Login
