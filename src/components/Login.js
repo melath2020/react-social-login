cb4d56ac2040dfc0ea0f5930317ac4d08868a85b
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-08 15:46:07 +0530

    updte

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,55 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import './loginstyle.css'
-import { GoogleLogin ,googleLogout} from '@react-oauth/google';
-
-
-
+import React, { useEffect, useState } from "react";
+import "./loginstyle.css";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
 const Login = () => {
- const [Success, setSuccess] = useState(false)
-  const [Logout, setLogout] = useState(false)
+  const [Success, setSuccess] = useState(false);
+  const [Logout, setLogout] = useState(false);
 
   useEffect(() => {
-    googleLogout()
-
-}, [Logout])
+    googleLogout();
+  }, [Logout]);
   return (
     <div>
-        <div class="container box-width shadow-sm pb-5" >
-            <div className="text-center">
-                <h5 className='mt-5 pt-3 mb-3'>Login Here</h5>
-                <div className="box bg-light-50  p-2 ">
-                    <input type="text" className='form-control' placeholder='UserName'/>
-                    <input type="text" className='form-control mt-3' placeholder='Password'/>
-                </div>
-                <hr />
-<div className="row">
-    <div className="col-md-6">
-    <GoogleLogin
-  onSuccess={credentialResponse => {
-
-    console.log(credentialResponse);
-    setSuccess(credentialResponse)
-    alert("Login Success")
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-/>;
-    </div>
-    <div className="col-md-6">
-          <button onClick={()=>setLogout(true)}>LogOut</button>
-
-    </div>
-</div>
-            
+      <div class="container box-width shadow-sm pb-5">
+        <div className="text-center">
+          <h5 className="mt-5 pt-3 mb-3">Login Here</h5>
+          <div className="box bg-light-50  p-2 ">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="UserName"
+            />
+            <input
+              type="text"
+              className="form-control mt-3"
+              placeholder="Password"
+            />
+          </div>
+          <hr />
+    
+           
+              <GoogleLogin
+                onSuccess={(credentialResponse) => {
+                  console.log(credentialResponse);
+                  setSuccess(credentialResponse);
+                  alert("Login Success");
+                }}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+              />
+              ;
             </div>
-          
-        </div>
-        
-      
+            
+       
+     
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
